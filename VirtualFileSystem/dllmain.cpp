9cbc00ccbfc830a494f68ffef0dcf47b88a8574e
usagirei <usagirei@users.noreply.github.com>
2017-01-18 17:05:45 -0200

    Better Voice Filtering, New Triggers

--- VirtualFileSystem/dllmain.cpp
+++ VirtualFileSystem/dllmain.cpp
@@ -20,11 +20,6 @@ typedef PDWORD(WINAPI *adDirect3DCreate9)(UINT sdkVersion);
 #define HK_ENTRYPOINT_SYMBOL_STR TOSTRING(HK_ENTRYPOINT_SYMBOL)
 HK_ENTRYPOINT_DELEGATE ogEntryPointCall;
 
-struct OggData {
-	uint8_t* Data;
-	uint32_t Size;
-};
-
 #define _DR1(base,offset) *(uint32_t*)((uint8_t*)base + offset)
 #define _DR2(b, o1, o2) _DR1(_DR1(b,o1),o2)
 #define _DR3(b, o1, o2, o3) _DR1(_DR2(b,o1,o2),o3)
@@ -32,16 +27,40 @@ struct OggData {
 #define _DR5(b, o1, o2, o3, o4, o5) _DR1(_DR4(b,o1,o2,o3,o4),o5)
 
 
+struct OggData {
+	uint8_t* Data;
+	uint32_t Size;
+};
 
-char loadedTpk[32] = { 0 };
-char* tpkData;
-OggData* oggData;
-int nOggFiles;
+struct {
+	uint8_t Option;
+	uint8_t MaxTries;
+	uint8_t Tries;
+	uint8_t MiniGame;
+	uint8_t Level;
+	char Girl[4];
+} GameState;
+
+struct {
+	OggData* OggDataA = nullptr;
+	OggData* OggDataB = nullptr;
+
+	int NumOggFilesA = 0;
+	int NumOggFilesB = 0;
+
+	char* TpkDataA = nullptr;
+	char* TpkDataB = nullptr;
+
+	uint32_t Delay = 0;
+} AudioState;
+
+int lastRand;
+char  lastModel[48];
 RamFS::Tracker* tpkTracker;
-uint32_t breathCooldown = 0;
 
-#define PLAY_REACT 0xFFFFFFFF
-#define PLAY_BREAT 0xFFFFFFFE
+#define TRIGGER_SUCCESS 0xFFFFFFFF
+#define TRIGGER_DIALOG 0xFFFFFFFE
+#define TRIGGER_FAILURE 0xFFFFFFFD
 
 extern "C" PDWORD WINAPI HK_ENTRYPOINT_SYMBOL(UINT sdkVersion) {
 
@@ -49,21 +68,45 @@ extern "C" PDWORD WINAPI HK_ENTRYPOINT_SYMBOL(UINT sdkVersion) {
 		return ogEntryPointCall(sdkVersion);
 	}
 
-	int nth = rand() % nOggFiles;
+	int num = lastRand + rand();
 	switch (sdkVersion) {
-		case PLAY_REACT:
-			tcout << "Playing Reaction Sound [" << nth << "]\n";
-			return (PDWORD)&oggData[nth];
-		case PLAY_BREAT:
-			if (breathCooldown == 0) {
-				breathCooldown = 60;
-				tcout << "Playing Breathing Sound [" << nth << "]\n";
-				return (PDWORD)&oggData[nth];
+		case TRIGGER_SUCCESS: 
+		{
+			int sid = (num % 4);
+			int id = (4 + sid);
+			OggData *data = &AudioState.OggDataB[id];
+			AudioState.Delay = (data->Size * 5) / 1024;
+			tcout << "Playing Success Sound [" << id << "," << AudioState.Delay << "]\n";
+			return (PDWORD)data;
+		}
+		case TRIGGER_FAILURE:
+		{
+			int sid = (num % 4);
+			int id = (8 + sid);
+			OggData *data = &AudioState.OggDataB[id];
+			AudioState.Delay = (data->Size * 6) / 1000;
+			tcout << "Playing Failure Sound [" << id << "," << AudioState.Delay << "]\n";
+			return (PDWORD)data;
+		}
+		case TRIGGER_DIALOG: 
+		{
+			if (AudioState.Delay == 0) {
+
+				int sid = (num % 16 < 8) 
+					? 2 + (num % 4)
+					: 0 + (num % 2);
+
+				int id = (6 * GameState.Option) + sid;
+				OggData *data = &AudioState.OggDataA[id];
+				AudioState.Delay = (data->Size * 6) / 1000;
+				tcout << "Playing " << ((sid >= 2) ? "Dialogue" : "Breathing") << " Sound [" << id << "," << AudioState.Delay << "]\n";
+				return (PDWORD)data;
 			}
 			else {
-				--breathCooldown;
+				--AudioState.Delay;
 				return nullptr;
 			}
+		}
 		default:
 			return nullptr;
 	}
@@ -84,59 +127,88 @@ void DetectTargetTPK(char* fName) {
 	if (cmp != 0)
 		return;
 
-	uint32_t ba = (uint32_t)GetModuleHandle(nullptr);
-	uint32_t qm = _DR5(ba, 0x001C9554, 0x1C, 0x54C, 0x5E8, 0x1E7C);
+	if (strcmp(fName, lastModel) == 0)
+		return;
+	strcpy_s(lastModel, fName);
 
-	char nBuf[4];
+	GameState.Option = _DR5(GetModuleHandle(nullptr), 0x1C9554, 0x1C, 0x54C, 0x5E8, 0x1E7C);
+	GameState.MaxTries = _DR2(GetModuleHandle(nullptr), 0x1C9560, 0xF0);
+
 	char ibuf[] = "x";
 	strncpy_s(buf, fName, len - 11);
 	len = strlen(buf);
 
 	ibuf[0] = *(buf + len - 1);
-	int lvl = atoi(ibuf) - 1;
+	GameState.Level = atoi(ibuf) - 1;
 
 	ibuf[0] = *(buf + len - 3);
-	int minigame = atoi(ibuf) - 1;
+	GameState.MiniGame = atoi(ibuf) - 1;
 
-	strncpy_s(nBuf, fName, 3);
+	strncpy_s(GameState.Girl, fName, 3);
 
-	if (minigame == 4)
-		lvl = 0;
+	if (GameState.MiniGame == 4)
+		GameState.Level = 0;
 
-	lvl = min(lvl, 2);
+	GameState.Level = min(GameState.Level, 2);
 
-	sprintf_s(buf, "pan_react_%s%02d_%d.tpk", nBuf, minigame, lvl);
-	if (strcmp(buf, loadedTpk) == 0)
-		return;
+	// Breathing - Banter TPK
 
-	strcpy_s(loadedTpk, buf);
-
-	tcout << "Loading TPK " << buf << "\n";
+	sprintf_s(buf, "pan_react_%s%02d_%d.tpk", GameState.Girl, GameState.MiniGame, GameState.Level);
+	tcout << "Loading TPK A " << buf << "\n";
+	AudioState.Delay = 180;
 
 	DWORD tpkRd;
 	uint64_t tpkSz;
 	uint64_t tpkPos = tpkTracker->FileSystem->FindEntryOffset(buf, &tpkSz);
-	tpkTracker->Seek(tpkPos, FILE_BEGIN);
-	tpkTracker->Read(tpkData, tpkSz, &tpkRd);
 
-	uint64_t nFiles = *(uint64_t*)(tpkData + 0x08);
+	tpkTracker->Seek(tpkPos, FILE_BEGIN);
+	tpkTracker->Read(AudioState.TpkDataA, tpkSz, &tpkRd);
+
+	uint64_t nFiles = *(uint64_t*)(AudioState.TpkDataA + 0x08);
 	uint64_t nFilesPad = (nFiles + 1) & ~1;
 
 	struct TPKENTRY {
 		char Name[40];
 		uint32_t Size;
 		uint32_t Offset;
-	}*entries = (TPKENTRY*)(tpkData + 16 + (2 * nFilesPad));
+	};
 
-	if (oggData)
-		delete[] oggData;
+	TPKENTRY* entries = (TPKENTRY*)(AudioState.TpkDataA + 16 + (2 * nFilesPad));
 
-	oggData = new OggData[nFiles];
-	nOggFiles = nFiles;
+	if (AudioState.OggDataA)
+		delete[] AudioState.OggDataA;
+
+	AudioState.OggDataA = new OggData[nFiles];
+	AudioState.NumOggFilesA = nFiles;
 
 	for (int i = 0; i < nFiles;++i) {
-		oggData[i].Data = (uint8_t*)(tpkData + entries[i].Offset);
-		oggData[i].Size = entries[i].Size;
+		AudioState.OggDataA[i].Data = (uint8_t*)(AudioState.TpkDataA + entries[i].Offset);
+		AudioState.OggDataA[i].Size = entries[i].Size;
+	}
+
+	// Reactions TPK
+
+	sprintf_s(buf, "pan_react_%s_%d.tpk", GameState.Girl, (3 * GameState.Option) + GameState.Level);
+	tcout << "Loading TPK B " << buf << "\n";
+
+	tpkPos = tpkTracker->FileSystem->FindEntryOffset(buf, &tpkSz);
+	tpkTracker->Seek(tpkPos, FILE_BEGIN);
+	tpkTracker->Read(AudioState.TpkDataB, tpkSz, &tpkRd);
+
+	nFiles = *(uint64_t*)(AudioState.TpkDataB + 0x08);
+	nFilesPad = (nFiles + 1) & ~1;
+
+	entries = (TPKENTRY*)(AudioState.TpkDataB + 16 + (2 * nFilesPad));
+
+	if (AudioState.OggDataB)
+		delete[] AudioState.OggDataB;
+
+	AudioState.OggDataB = new OggData[nFiles];
+	AudioState.NumOggFilesB = nFiles;
+
+	for (int i = 0; i < nFiles;++i) {
+		AudioState.OggDataB[i].Data = (uint8_t*)(AudioState.TpkDataB + entries[i].Offset);
+		AudioState.OggDataB[i].Size = entries[i].Size;
 	}
 }
 
@@ -161,26 +233,35 @@ void Initialize() {
 	tcout << "Git Commit Hash: " << GIT_COMMIT_HASH << "\n";
 	tcout << "Git Commit Date: " << GIT_COMMIT_DATE << "\n\n";
 
-	tcout << "Applying Patches...\n";
-	ApplyPatches();
+	//tcout << "Applying Patches...\n";
+	//ApplyPatches();
 
 	srand(time(nullptr));
 
 	tpkTracker = nullptr;
-	oggData = nullptr;
-	nOggFiles = 0;
-	tpkData = nullptr;
 
 	tcout << "Initializing Virtual File System...\n";
 	VirtualFileSystem::Initialize();
 
+	//TODO: Find Way to detect which locale is loaded
+
+	AudioState.TpkDataA = new char[4 * 1024 * 1024];
+	AudioState.TpkDataB = new char[4 * 1024 * 1024];
+
 	if (VirtualFileSystem::DataEn) {
 		VirtualFileSystem::DataEn->OnFileRead = &DetectTargetTPK;
 		tpkTracker = VirtualFileSystem::DataEn->CreateTracker();
-		tpkData = new char[4 * 1024 * 1024];
 	}
 	else {
-		tcout << "Couldn't Initialize Virtual File System!\n";
+		tcout << "Couldn't Initialize En Virtual File System!\n";
+	}
+
+	if (VirtualFileSystem::DataJp) {
+		VirtualFileSystem::DataJp->OnFileRead = &DetectTargetTPK;
+		//tpkTracker = VirtualFileSystem::DataJp->CreateTracker();
+	}
+	else {
+		tcout << "Couldn't Initialize JP Virtual File System!\n";
 	}
 
 }
